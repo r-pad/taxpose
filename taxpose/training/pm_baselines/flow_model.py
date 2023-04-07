1a527855b4e8695eb085659da23914e281d58f35
Ben Eisner <ben.a.eisner@gmail.com>
2023-04-07 15:01:15 -0400

    consolidate model

--- taxpose/training/pm_baselines/flow_model.py
+++ taxpose/training/pm_baselines/flow_model.py
@@ -1,10 +1,14 @@
+from dataclasses import dataclass
 from typing import Dict
 
 import plotly.graph_objects as go
 import pytorch_lightning as pl
+import rpad.pyg.nets.mlp as pnm
 import rpad.pyg.nets.pointnet2 as pnp
 import rpad.visualize_3d.plots as pvp
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as opt
 import torch_geometric.data as tgd
 from plotly.subplots import make_subplots
@@ -12,7 +16,6 @@ from torch_geometric.data.batch import Batch
 from torch_geometric.data.data import Data
 
 from taxpose.datasets.pm_placement import CATEGORIES
-from taxpose.training.pm_baselines.naive_nets import FRNetCLIPort, GoalInfFlowNetParams
 
 
 def artflownet_loss(
@@ -110,8 +113,157 @@ def flow_plot(
     return fig
 
 
+@dataclass
+class FRNetCLIPortParams:
+    in_dim: int = 1
+    sa1: pnp.SAParams = pnp.SAParams(0.2, 0.2, pnm.MLPParams((64, 64)))
+    sa1_outdim: int = 128
+
+    sa2: pnp.SAParams = pnp.SAParams(0.25, 0.4, pnm.MLPParams((128, 128)))
+    sa2_outdim: int = 256
+
+    gsa: pnp.GlobalSAParams = pnp.GlobalSAParams(pnm.MLPParams((256, 512)))
+    gsa_outdim: int = 1024
+
+    # Parameters for the Feature Propagation modules.
+    fp3: pnp.FPParams = pnp.FPParams(pnm.MLPParams((256, 256)), k=1)
+    fp2: pnp.FPParams = pnp.FPParams(pnm.MLPParams((256, 128)))
+    fp1: pnp.FPParams = pnp.FPParams(pnm.MLPParams((256, 128)))
+    fp1_outdim: int = 128
+
+    # Dimensions of the final 2 linear layers.
+    lin1_dim: int = 128
+    lin2_dim: int = 128
+
+    # Final output dim
+    final_outdim: int = 3
+
+
+class FRNetCLIPort(nn.Module):
+    def __init__(self, p: FRNetCLIPortParams):
+        super().__init__()
+
+        # The Set Aggregation modules.
+        self.sa1 = pnp.SAModule(3 + p.in_dim, p.sa1_outdim, p=p.sa1)
+        self.sa2 = pnp.SAModule(3 + p.sa1_outdim, p.sa2_outdim, p=p.sa1)
+        self.sa3 = pnp.GlobalSAModule(3 + p.sa2_outdim, p.gsa_outdim, p=p.gsa)
+
+        # The Feature Propagation modules.
+        self.fp3 = pnp.FPModule(p.gsa_outdim + p.sa2_outdim, p.sa2_outdim, p.fp3)
+        self.fp2 = pnp.FPModule(p.sa2_outdim + p.sa1_outdim, p.sa1_outdim, p.fp2)
+        self.fp1 = pnp.FPModule(p.sa1_outdim + p.in_dim, p.fp1_outdim, p.fp1)
+
+        # Final linear layers at the output.
+        self.lin1 = torch.nn.Linear(p.fp1_outdim, p.lin1_dim)
+        self.lin2 = torch.nn.Linear(p.lin1_dim, p.lin2_dim)
+        self.lin3 = torch.nn.Linear(p.lin2_dim, 3)  # Flow output.
+
+    def forward(self, data, flows):
+        sa0_out = (data.x, data.pos.float(), data.batch)
+
+        sa1_out = self.sa1(*sa0_out)
+        sa2_out = self.sa2(*sa1_out)
+        x3, pos3, batch3 = self.sa3(*sa2_out)
+
+        # CLIPort network uses tiling then hadamard product at each subsequent layer.
+        x3 = torch.mul(x3, flows.tile(1, 8))
+        sa3_out = x3, pos3, batch3
+
+        fp3_x, fp3_pos, fp3_batch = self.fp3(*sa3_out, *sa2_out)
+        temp = torch.cat(
+            [
+                flows[i].tile(int(fp3_x.shape[0] / flows.shape[0]), 2)
+                for i in range(flows.shape[0])
+            ]
+        )
+        if temp.shape[0] != fp3_x.shape[0]:
+            temp = torch.cat([temp, flows[: fp3_x.shape[0] - temp.shape[0]].tile(1, 2)])
+        fp3_x = torch.mul(fp3_x, temp)
+        fp3_out = fp3_x, fp3_pos, fp3_batch
+        fp2_x, fp2_pos, fp2_batch = self.fp2(*fp3_out, *sa1_out)
+        temp = torch.cat(
+            [
+                flows[i].tile(int(fp2_x.shape[0] / flows.shape[0]), 1)
+                for i in range(flows.shape[0])
+            ]
+        )
+        if temp.shape[0] != fp2_x.shape[0]:
+            temp = torch.cat([temp, flows[: fp2_x.shape[0] - temp.shape[0]].tile(1, 1)])
+        fp2_x = torch.mul(fp2_x, temp)
+        fp2_out = fp2_x, fp2_pos, fp2_batch
+        x, _, _ = self.fp1(*fp2_out, *sa0_out)
+
+        x = F.relu(self.lin1(x))
+        x = F.relu(self.lin2(x))
+        x = self.lin3(x)
+
+        return x
+
+
+class FRNetCLIPortProjection(nn.Module):
+    def __init__(self, p: FRNetCLIPortParams):
+        super().__init__()
+
+        # The Set Aggregation modules.
+        self.sa1 = pnp.SAModule(3 + p.in_dim, p.sa1_outdim, p=p.sa1)
+        self.sa2 = pnp.SAModule(3 + p.sa1_outdim, p.sa2_outdim, p=p.sa1)
+        self.sa3 = pnp.GlobalSAModule(3 + p.sa2_outdim, p.gsa_outdim, p=p.gsa)
+
+        # The Feature Propagation modules.
+        self.fp3 = pnp.FPModule(p.gsa_outdim + p.sa2_outdim, p.sa2_outdim, p.fp3)
+        self.fp2 = pnp.FPModule(p.sa2_outdim + p.sa1_outdim, p.sa1_outdim, p.fp2)
+        self.fp1 = pnp.FPModule(p.sa1_outdim + p.in_dim, p.fp1_outdim, p.fp1)
+
+        # Final linear layers at the output.
+        self.lin1 = torch.nn.Linear(p.fp1_outdim, p.lin1_dim)
+        self.lin2 = torch.nn.Linear(p.lin1_dim, p.lin2_dim)
+        self.lin3 = torch.nn.Linear(p.lin2_dim, p.final_outdim)  # Flow output.
+        self.proj1 = torch.nn.Linear(p.sa2_outdim // 2, p.sa2_outdim)
+        self.proj2 = torch.nn.Linear(p.sa1_outdim, p.sa1_outdim)
+
+    def forward(self, data, flows):
+        sa0_out = (data.x, data.pos.float(), data.batch)
+
+        sa1_out = self.sa1(*sa0_out)
+        sa2_out = self.sa2(*sa1_out)
+        x3, pos3, batch3 = self.sa3(*sa2_out)
+
+        # CLIPort network uses tiling then hadamard product at each subsequent layer.
+        x3 = torch.mul(x3, flows.tile(1, 8))
+        sa3_out = x3, pos3, batch3
+
+        fp3_x, fp3_pos, fp3_batch = self.fp3(*sa3_out, *sa2_out)
+        temp = torch.cat(
+            [flows[i].tile((fp3_batch == i).sum(), 2) for i in range(flows.shape[0])]
+        )
+        fp3_x = torch.mul(fp3_x, temp)
+        fp3_out = fp3_x, fp3_pos, fp3_batch
+        fp2_x, fp2_pos, fp2_batch = self.fp2(*fp3_out, *sa1_out)
+        temp = torch.cat(
+            [flows[i].tile((fp2_batch == i).sum(), 1) for i in range(flows.shape[0])]
+        )
+        fp2_x = torch.mul(fp2_x, temp)
+        fp2_out = fp2_x, fp2_pos, fp2_batch
+        x, _, _ = self.fp1(*fp2_out, *sa0_out)
+
+        x = F.relu(self.lin1(x))
+        x = F.relu(self.lin2(x))
+        x = self.lin3(x)
+
+        return x
+
+
+@dataclass
+class FlowNetParams:
+    in_dim: int = 0
+    flow_embed_dim: int = 128
+    gfe_net: pnp.PN2EncoderParams = pnp.PN2EncoderParams()
+    fr_net: FRNetCLIPortParams = FRNetCLIPortParams()
+    inference: bool = False
+
+
 class FlowNet(pl.LightningModule):
-    def __init__(self, p: GoalInfFlowNetParams = GoalInfFlowNetParams()):
+    def __init__(self, p: FlowNetParams = FlowNetParams()):
         super().__init__()
 
         self.gfe_net = pnp.PN2Encoder(in_dim=1, out_dim=p.flow_embed_dim, p=p.gfe_net)
