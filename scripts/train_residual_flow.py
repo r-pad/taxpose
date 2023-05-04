2902495d99489ea65ac282b5de92c93ba4961b4f
Ben Eisner <ben.a.eisner@gmail.com>
2023-05-03 23:36:34 -0400

    add new system for path and logging (#11)

--- scripts/train_residual_flow.py
+++ scripts/train_residual_flow.py
@@ -35,8 +35,12 @@ def main(cfg):
         max_epochs=cfg.max_epochs,
     )
     log_txt_file = cfg.log_txt_file
-    write_to_file(log_txt_file, "working_dir: {}".format(os.getcwd()))
-    write_to_file(log_txt_file, "")
+    if cfg.mode == "train":
+        write_to_file(log_txt_file, "-----------------------")
+        write_to_file(log_txt_file, "Project: {}".format(logger._project))
+        write_to_file(log_txt_file, "Experiment: {}".format(logger.experiment.name))
+        write_to_file(log_txt_file, "working_dir: {}".format(os.getcwd()))
+        write_to_file(log_txt_file, "")
     dm = MultiviewDataModule(
         dataset_root=hydra.utils.to_absolute_path(cfg.train_data_dir),
         test_dataset_root=hydra.utils.to_absolute_path(cfg.test_data_dir),
@@ -85,7 +89,7 @@ def main(cfg):
 
     model.cuda()
     model.train()
-    if cfg.checkpoint_file is not None:
+    if cfg.load_from_checkpoint:
         print("loaded checkpoint from")
         print(cfg.checkpoint_file)
         model.load_state_dict(
@@ -121,8 +125,28 @@ def main(cfg):
                     cfg.task.checkpoint_file_anchor
                 )
             )
+    if cfg.mode == "train":
+        trainer.fit(model, dm)
+    elif cfg.mode == "train_val":
+        pl.seed_everything(123456)
+        model.eval()
 
-    trainer.fit(model, dm)
+        with torch.no_grad():
+            results = trainer.validate(model, dm.train_dataloader())
+    elif cfg.mode == "validate":
+        pl.seed_everything(123456)
+        model.eval()
+
+        with torch.no_grad():
+            results = trainer.validate(model, dm)
+    elif cfg.mode == "test":
+        pl.seed_everything(123456)
+        model.eval()
+
+        with torch.no_grad():
+            results = trainer.test(model, dm)
+    else:
+        raise ValueError("Mode not recognized")
 
 
 if __name__ == "__main__":
