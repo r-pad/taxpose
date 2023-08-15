9c4871e7cd61d9f17e343dbcb00769d9058bd18f
Ben Eisner <ben.a.eisner@gmail.com>
2023-08-14 23:39:52 -0400

    new reupload command

--- scripts/reupload_artifact.py
+++ scripts/reupload_artifact.py
@@ -5,6 +5,6 @@ path = "/opt/logs/train_mug_place/2023-08-14/16-50-36/checkpoints/last.ckpt"
 
 
 with wandb.init(id=run_id, resume="allow") as run:
-    artifact = wandb.Artifact(f"model-{run_id}:v0", "model")
+    artifact = wandb.Artifact(f"model-{run_id}", "model")
     artifact.add_file(path)
     run.log_artifact(artifact)
