70a5f1f8a9ecb94d5926fd53049ced7e3dec62ff
Ben Eisner <ben.a.eisner@gmail.com>
2024-05-14 11:05:18 -0400

    Beneisner/add rlbench eval (#51)
    * add in the rlbench eval
    
    * rlbench evals seem to work
    
    * fold in remaining rlbench utils for cluster training

---
+++ copy_rlbench_and_unzip.sh
@@ -0,0 +1,12 @@
+# Copy the folder named rlbench.zip from /project_data/held/baeisner/rlbench.zip to /scratch/baeisner/data/rlbench.zip
+# Unzip the file rlbench.zip in /scratch/baeisner/data/rlbench
+# Delete the file rlbench.zip in /scratch/baeisner/data/rlbench.zip
+
+# Rsync with progress
+rsync -av --progress /project_data/held/baeisner/rlbench.zip /scratch/baeisner/data/rlbench.zip
+
+# Unzip
+unzip /scratch/baeisner/data/rlbench.zip -d /scratch/baeisner/data
+
+# Delete
+rm /scratch/baeisner/data/rlbench.zip
